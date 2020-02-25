c57814db577e3b1601f9274efcda5f7ead344de7
val <val@vals-MacBook-Pro.local>
2020-02-25 00:23:05 +0000

    now input field has a value

--- src/Components/SearchPage/SearchPage.js
+++ src/Components/SearchPage/SearchPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { PureComponent } from "react";
 import styled from "styled-components";
 
 const SearchPageForm = styled.form`
@@ -14,20 +14,46 @@ const SearchPageInput = styled.input`
   border: 1px #ccc solid;
 `;
 
-export const SearchPage = () => {
-  return (
-    <SearchPageForm>
-      <SearchPageInput type="text" name="ingredient" placeholder="Ingredient" />
-      <div>
-        <h3>Recent Searches</h3>
-        <ol>
-          <li>Search History 1</li>
-          <li>Search History 2</li>
-        </ol>
-      </div>
-      <input type="submit" value="Submit" />
-    </SearchPageForm>
-  );
-};
+class SearchPage extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert("Ingredient submitted: " + this.state.value);
+
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <SearchPageForm>
+        <SearchPageInput
+          value={this.state.value}
+          onChange={this.handleChange}
+          placeholder="Ingredient"
+        />
+        <div>
+          <h3>Recent Searches</h3>
+          <ol>
+            <li>Search History 1</li>
+            <li>Search History 2</li>
+          </ol>
+        </div>
+        <input type="submit" value="Submit" />
+      </SearchPageForm>
+    );
+  }
+}
 
 export default SearchPage;
