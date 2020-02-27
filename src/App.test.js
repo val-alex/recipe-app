2c014908f2df99f1642e8d5fb02e1f7b2e9a6313
val <val@vals-MacBook-Pro.local>
2020-02-27 12:32:26 +0000

    setting up tests, work in progress, all test are failing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,28 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import "jest-enzyme";
+
+import App from "./App";
+
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+describe("(component) App", () => {
+  let wrapper;
+  let instance;
+
+  beforeEach(() => {
+    const requiredProps = {};
+
+    wrapper = shallow(<App {...requiredProps} />);
+    instance = wrapper.instance();
+  });
+
+  it("", () => {
+    expect(wrapper).toContainReact(welcome);
+  });
 });
