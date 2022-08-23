7bb1f67f3557ca872541f172c4bd892dcafe3ccd
Valentin Alexeev <valentin.m.alexeev@gmail.com>
2022-08-23 12:23:20 +0300

    moved from formik to react hook form

--- src/components/IngredientsForm/IngredientsForm.js
+++ src/components/IngredientsForm/IngredientsForm.js
@@ -1,89 +1,49 @@
 import { useRouter } from "next/router";
 
-import { Formik } from "formik";
+import { useForm } from "react-hook-form";
 import { useDispatch } from "react-redux";
-import * as Yup from "yup";
 
 import { getRecipes, setRecentSearches } from "@store/Reducers/recipesSlice";
 
 import * as Styles from "./IngredientsFormStyles";
 
-const validationSchema = Yup.object().shape({
-  ingredients: Yup.string()
-    .min(3, "ingredients must have at least 3 letters")
-    .required("Must enter an ingredient"),
-});
-
-export const Error = ({ touched, message }) => {
-  if (!touched) {
-    return <Styles.FormError>&nbsp;</Styles.FormError>;
-  }
-
-  if (message) {
-    return <Styles.FormError>{message}</Styles.FormError>;
-  }
-
-  return <Styles.FormValid>Valid</Styles.FormValid>;
-};
-
 export const IngredientsForm = ({ children }) => {
   const dispatch = useDispatch();
   let router = useRouter();
 
-  const handleSubmit = ({ ingredients }, { setSubmitting, resetForm }) => {
-    setSubmitting(true);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
+  const onSubmit = ({ ingredients }) => {
     dispatch(setRecentSearches(ingredients));
     dispatch(getRecipes(ingredients));
-
-    resetForm();
-    setSubmitting(false);
 
     router.push("/recipes");
   };
 
   return (
-    <Formik
-      initialValues={{ ingredients: "" }}
-      validationSchema={validationSchema}
-      onSubmit={handleSubmit}
-    >
-      {({
-        values,
-        errors,
-        touched,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        isSubmitting,
-      }) => (
-        <form onSubmit={handleSubmit}>
-          <Styles.FormInputWrapper>
-            <Styles.FormLabel htmlFor="ingredients">
-              Ingredients:
-            </Styles.FormLabel>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Styles.FormInputWrapper>
+        <Styles.FormLabel htmlFor="ingredients">Ingredients:</Styles.FormLabel>
 
-            <Styles.FormInput
-              type="text"
-              name="ingredients"
-              if="ingredients"
-              placeholder="Enter your ingredients"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.ingredients}
-            />
+        <Styles.FormInput
+          defaultValue=""
+          {...register("ingredients", { required: true })}
+          placeholder="Enter your ingredients"
+        />
 
-            <Error touched={touched.ingredients} message={errors.ingredients} />
+        {errors.ingredients ? (
+          <span>Please enter 1 or more ingredients</span>
+        ) : null}
 
-            {children ? <div>{children}</div> : null}
+        {children ? <div>{children}</div> : null}
 
-            <Styles.FormButton type="submit" disabled={isSubmitting}>
-              Search
-            </Styles.FormButton>
-          </Styles.FormInputWrapper>
-        </form>
-      )}
-    </Formik>
+        <Styles.FormButton type="submit">Search</Styles.FormButton>
+      </Styles.FormInputWrapper>
+    </form>
   );
 };
 
